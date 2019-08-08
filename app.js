e851b91881a7fd0014ec4199548911b6f8dc248e
Tim Moua <49731829+tjmoua19@users.noreply.github.com>
2019-08-08 18:08:51 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,48 @@
+window.onload = function() {
+	alert("The window has loaded!");
+};
+
+function onReady() {
+	const addToDoForm = document.getElementById('addToDoForm');
+	const newToDoText = document.getElementById('newToDoText');
+	const toDoList = document.getElementById('toDoList'); 
+
+	addToDoForm.addEventListener('submit', (event) => {
+		event.preventDefault();
+
+		// get the text 
+		let title = newToDoText.value; 
+
+		// create a new li
+		let newLi = document.createElement('li');
+
+		// create a new input
+		let checkbox = document.createElement('input');
+
+		// set the input's type to checkbox
+		checkbox.type = "checkbox";
+
+		let deleteBtn = document.createElement('button');
+		deleteBtn.textContent = "Delete";
+
+		deleteBtn.addEventListener('click', function(event){
+			// console.log(event);
+
+			toDoList.removeChild(this.parentElement);
+		})
+
+		// set the title 
+		newLi.textContent = title; 
+		// attach the checkbox to the li
+		newLi.appendChild(checkbox);
+		newLi.appendChild(deleteBtn);
+		// attach the li to the ul
+		toDoList.appendChild(newLi);
+		// empty the input
+		newToDoText.value = '';
+	});
+}
+
+window.onload = function() {
+	onReady();
+}
