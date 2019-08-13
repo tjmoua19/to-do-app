b177bb5d32b784f37d1bea5727fdbc7b969102c4
Tim Moua <49731829+tjmoua19@users.noreply.github.com>
2019-08-13 15:49:06 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,84 +1,42 @@
 function onReady() {
-  let toDos = JSON.parse( localStorage.getItem('toDos') ) || [];
-  let id = toDos.length || 0;
-  const addToDoForm = document.getElementById('addToDoForm');
-  const newToDoText = document.getElementById('newToDoText');
+    let id = 0;
+    var toDos = [];
+    const addToDoForm = document.getElementById("addToDoForm");
 
-  function createNewToDo() {
-    const newToDoText = document.getElementById("newToDoText");
-    if (!newToDoText.value) {
-      return;
-  }
-
-  toDos.push({
-    title: newToDoText.value,
-    complete: false,
-    id: id
-  });
-    newToDoText.value = "";
-    renderTheUI();
-    id = id++;
-  }
-
-  function deleteToDo(id) {
-    return toDos.filter(toDo => toDo.id !== id);
-  }
-
-  function saveToDos() {
-    localStorage.setItem('toDos', JSON.stringify(toDos) );
-  }
-
-
-  function deleteToDo(id) {
-        return toDos.filter(toDo => toDo.id !== id);
-    }
-
-    function saveToDos() {
-        localStorage.setItem('toDos', JSON.stringify(toDos) );
+    function createNewToDo() {
+        const newToDoText = document.getElementById("newToDoText");
+        if (!newToDoText.value) {
+            return;
+        }
+        toDos.push({
+            title: newToDoText.value,
+            complete: false,
+            id: id
+        });
+        newToDoText.value = "";
+        renderTheUI();
+        id = id + 1;
     }
 
     function renderTheUI() {
         const toDoList = document.getElementById("toDoList");
-
         toDoList.textContent = '';
-
         toDos.forEach(function (toDo) {
             const newLi = document.createElement('li');
             const checkbox = document.createElement('input');
-            
+            //newLi.className = "task";
             checkbox.type = "checkbox";
-
-      checkbox.checked = toDo.complete;
-
-      
-      const deleteButton = document.createElement('button');
-        deleteButton.textContent = "Delete";
-        
-        deleteButton.addEventListener('click', () => {
-        toDos = toDos.filter(function(task){
-        return task.id !== toDo.id;
-      });
-
-      checkbox.addEventListener('click', function() {
-        toDo.complete = checkbox.checked ? true : false;
-        saveToDos();
-      });
-
-      toDoList.appendChild(newLi);
-      newLi.appendChild(checkbox);
-      newLi.appendChild(deleteBtn);
-
-      deleteBtn.addEventListener('click', () => {
-        toDos = deleteToDo(toDo.id);
-        renderTheUI();
-        saveToDos();
-      });
-        
-      renderTheUI();
-    });
-            
+            const deleteButton = document.createElement('button');
+            deleteButton.textContent = "Delete";
+            deleteButton.addEventListener('click', () => {
+                toDos = toDos.filter(function(task){
+                    return task.id !== toDo.id;
+                });
+                renderTheUI();
+            });
+            //add the toDo's title text to newLi
             newLi.textContent = toDo.title;
-          
+            //update the DOM
             toDoList.appendChild(newLi);
             newLi.appendChild(checkbox);
             newLi.appendChild(deleteButton);
@@ -90,10 +48,9 @@ function onReady() {
         createNewToDo();
     });
 
-  renderTheUI();
+    renderTheUI();
 }
 
 window.onload = function () {
     onReady();
 };
-
